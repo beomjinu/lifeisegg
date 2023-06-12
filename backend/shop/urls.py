7625918dab297cbb5a2191ff2c697c1b1e2f510f
beomjinu <ulhangry.dev@gmail.com>
2023-06-13 00:45:04 +0900

    detail 페이지 거의 완성

--- backend/shop/urls.py
+++ backend/shop/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'shop'
+
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('', views.index, name='index'),
+    path('<int:product_id>/', views.detail, name='detail'),
 ]
--- backend/shop/views.py
+++ backend/shop/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
+import json
 
 def index(request):
     products = Product.objects.order_by('priority')
@@ -9,8 +10,22 @@ def index(request):
         product.format_discounted = format(product.discounted, ",")
         product.solted_images     = product.images.all().order_by('priority')
 
-    context  = {
+    context = {
         'products': products
     }
 
-    return render(request, 'shop/index.html', context=context)
+    return render(request, 'shop/index.html', context=context)    
+
+def detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+
+    product.format_price      = format(product.price, ",")
+    product.format_discounted = format(product.discounted, ",")
+    product.solted_images     = product.images.all().order_by('priority')
+    product.solted_options    = product.options.all().order_by('priority')
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'shop/detail.html', context=context)
