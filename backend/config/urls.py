3d9e051402b2ee8ff6d8117d4ec893aac54f3eae
beomjinu <ulhangry.dev@gmail.com>
2023-06-13 03:35:04 +0900

    루트 경로로 진입시 shop/으로 redirect

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from shop.views import redirect_index
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('adminpage/', admin.site.urls),
     path('shop/', include("shop.urls")),
+    path('', redirect_index)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
