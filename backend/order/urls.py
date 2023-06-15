cde9347ef22dae3f3b71700371e97a6a805db12f
beomjinu <ulhangry.dev@gmail.com>
2023-06-16 01:15:28 +0900

    기본 틀 구성

--- backend/order/urls.py
+++ backend/order/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'order'
 
 urlpatterns = [
+    path('form/', views.form, name='form'),
 ]
--- backend/order/views.py
+++ backend/order/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+def form(request):
+    use_cart = bool(int(request.GET.get('use_cart')))
+
+    if use_cart:
+        pass
+    else:
+        pass
