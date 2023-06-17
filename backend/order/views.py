a4a16f5623c33aa863248fc89ae10a24041c8cb8
beomjinu <ulhangry.dev@gmail.com>
2023-06-17 16:03:39 +0900

    order form 일부 완성

--- backend/order/views.py
+++ backend/order/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from shop.models import Option
+import base64, json
 
 def form(request):
-    use_cart = bool(int(request.GET.get('use_cart')))
+    data = request.GET.get('data')
+    options = json.loads(base64.b64decode(data).decode('ascii'))
 
-    if use_cart:
-        pass
-    else:
-        pass
+    order_product = []
+
+    for i in options:
+        option = get_object_or_404(Option, pk=int(i))
+        option.quantity = options[i]["quantity"]
+        option.format_price = format((option.price + option.product.discounted) * option.quantity, ',')
+    
+        order_product.append(option)
+
+    context = {
+        'order_product': order_product
+    }
+
+    return render(request, 'order/form.html', context)
