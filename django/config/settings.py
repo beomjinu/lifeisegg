d5b74920ac3ff2a8c4e5b0bd818cdf4dfd66ee87
beomjinu <ulhangry.dev@gmail.com>
2023-06-27 03:40:13 +0900

    db 분리

--- django/config/settings.py
+++ django/config/settings.py
@@ -75,12 +75,24 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.environ.get('debug') == 'True':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get('db_name'),
+            'USER': os.environ.get('db_user'),
+            'PASSWORD': os.environ.get('db_password'),
+            'HOST': os.environ.get('db_host'),
+            'PORT': os.environ.get('db_port'),
+        }
+    }
 
 
 # Password validation
