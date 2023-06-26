75258ccaf5d8b3a892bf7f29e9f0409dff091a89
beomjinu <ulhangry.dev@gmail.com>
2023-06-27 00:26:49 +0900

    set static_root

--- django/config/settings.py
+++ django/config/settings.py
@@ -133,6 +133,7 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL   = "media/"
