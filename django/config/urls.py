c30b2cb6b001852c3c80e9c191c18b6c6ec7ae28
beomjinu <me@beomjinu.com>
2023-07-16 22:21:44 +0900

    Feat: Create 404.html file

--- django/config/urls.py
+++ django/config/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.sitemaps.views import sitemap
+from django.shortcuts import render
 
 from app.shop.views import redirect_index
 
@@ -27,5 +28,9 @@ urlpatterns = [
     path('cart/', include('app.cart.urls')),
     
     # sitemaps
-    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
+    path('sitemap.xml', sitemap, {'sitemaps': {'Product': ProductSitemap, 'Trems': TermsSitemap}}, name='sitemap'),
+
+    # 404
+    path('404.html', lambda request: render(request, '404.html', {}))
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
