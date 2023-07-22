38f3b681a6bdcfa4ec149b8050e21666a4094743
beomjinu <me@beomjinu.com>
2023-07-23 03:12:43 +0900

    Edit: log file max 크기 수정

--- django/config/settings.py
+++ django/config/settings.py
@@ -191,7 +191,7 @@ LOGGING = {
             'filters': ['require_debug_false'],
             'class': 'logging.handlers.RotatingFileHandler',
             'filename': BASE_DIR / 'logs/lifeisegg.log',
-            'maxBytes': 1024*1024*100,  # 100 MB
+            'maxBytes': 1024*1024*20,  # 20 MB
             'backupCount': 5,
             'formatter': 'standard',
         },
