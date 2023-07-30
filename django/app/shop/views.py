6c828bb7ac1d2547c52a4803c53942b86808daf8
beomjinu <me@beomjinu.com>
2023-07-30 22:03:45 +0900

    Edit: title, description 문구 변경

--- django/app/shop/views.py
+++ django/app/shop/views.py
@@ -5,7 +5,7 @@ def index(request):
     products = Product.objects.order_by('priority')
 
     context = {
-        'title': '라이프이즈에그 보드샵',
+        'title': '라이프이즈에그 스케이트보드',
         'products': products
     }
 
@@ -15,7 +15,7 @@ def detail(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
 
     context = {
-        'title': f'{product.name} - 라이프이즈에그',
+        'title': f'{product.name} 구매하기 - 라이프이즈에그',
         'description': f'{product.name} {format(product.discounted, ",")}원에 구매하기', 
         'product': product,
         'og_img_url': product.solted_images.first().image.url if not product.og_img else product.og_img.url
