716d5ac809794ae1fa70a0a689d9c20bea0fc973
beomjinu <me@beomjinu.com>
2023-08-01 00:48:51 +0900

    Feat(SEO): naver site verification
    네이버 서치어드바이저 사용을 위한 소유권 확인

--- django/config/urls.py
+++ django/config/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.sitemaps.views import sitemap
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 from app.shop.sitemaps import ProductSitemap
 from app.terms.sitemaps import TermsSitemap
@@ -24,13 +24,16 @@ urlpatterns = [
     path('payment/', include('app.payment.urls')),
     path('cart/', include('app.cart.urls')),
     
-    # sitemaps
+    # sitemaps.xml
     path('sitemap.xml', sitemap, {'sitemaps': {'Product': ProductSitemap, 'Trems': TermsSitemap}}, name='sitemap'),
 
-    # 404
+    # 404.html
     path('404.html', lambda request: render(request, '404.html')),
 
     # robots.txt
     path('robots.txt', lambda request: render(request, 'robots.txt', content_type='text/plain')),
+
+    # naver site verification
+    path('naver19af3aceb3c4654e177ebb055319e0b3.html', lambda _: HttpResponse('naver-site-verification: naver19af3aceb3c4654e177ebb055319e0b3.html'))
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
